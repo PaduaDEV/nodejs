6bd45457e19a2a1498e4ba44aa0a86b05f0bb264
DEVPADUA <arthurpadua100@gmail.com>
2023-07-09 14:31:17 -0700

    nodejs

---
+++ src/pessoa/PessoaVo.ts
@@ -0,0 +1,14 @@
+class PessoaVo
+
+{
+ public nome  : string = "";
+ public sobrenome : string = "";
+ public idade : number = 0;
+ public login : string = "";
+ public senha : number = 0;
+ public status : number = 0;
+ public enderecos : string = "";
+
+}
+
+export default PessoaVo
